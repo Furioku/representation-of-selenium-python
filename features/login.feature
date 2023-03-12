6cb3be176f09b074f2fad2f1d8a83a272a7e2baa
Philipp <94870193+Furioku@users.noreply.github.com>
2023-03-12 23:11:35 +0100

    Develop2 (#7)
    * feat: First implementation
    
    * feat: finishing of the login tsts
    
    * fix: uncomment headless mode
    
    * fix: gitignore

--- features/login.feature
+++ features/login.feature
@@ -1,17 +1,18 @@
 Feature: We would like to check login page:
-1. Please verify if coo
+develop2
+1. Please verify if cookies banner appears and work correctly
 2. Check if loging feature works
 
-Scenario: User can login into the appliccation
+Scenario Outline: Scenario Outline name: User can login into the appliccation
     Given User start at login page
-    Then User is able to see cookies information banner
-    # When User press Only Allow Essential Cookies button
-    # Then User is able to see email and password input fields
-    # When User type proper <email> into email input field
-    # And User type proper <password> into password input field
-    # And User click on Logg inn button
-    # Then User should land on application home page
-    # Examples:
-    #     | email                         | password           |
-    #     | philippsalata4@gmail.com      | MaRPhF39$97!22     |
-    #     | philippsalata3@gmail.com      | Lkdygc$SE$         |
+    Then User is able to see cookies information banner and verify text in title banner
+    When User press Only Allow Essential Cookies button
+    Then User is able to see email and password input fields
+    When User type proper <email> into email input field
+    And User type proper <password> into password input field
+    And User click on Logg inn button
+    Then User should land on application home page
+    Examples:
+        | email                         | password           |
+        | philippsalata4@gmail.com      | MaRPhF39$97!22     |
+        | philippsalata3@gmail.com      | Lkdygc$SE$         |
--- features/steps/login_page.py
+++ features/steps/login_page.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helpers import *
 from url_list import *
-from selenium.webdriver.chrome.options import Options
+develop2
+from test_data import *
 from environment import *
 
 COOKIE_BANNER_TITLE_TXT = "//div[@id = 'consent_cookies_title']"
@@ -18,6 +19,31 @@ LOGIN_BTN = "//button[@id = 'loginbutton']"
 def open_login_page(context):
     open_page(context, login_page_url)
 
-@then('User is able to see cookies information banner')
+
+@then('User is able to see cookies information banner and verify text in title banner')
 def cookies_banner_present(context):
     visibility(context, COOKIE_BANNER_TITLE_TXT)
+    text_from_cookies_banner_title = context.driver.find_element(By.XPATH, COOKIE_BANNER_TITLE_TXT).text
+    assert str(text_from_cookies_banner_title) == TEXT_IN_COOKIES_BANNER_TITLE
+
+@when('User press Only Allow Essential Cookies button')
+def press_on_allow_essential_btn(context):
+    click(context, ONLY_ESSENTIAL_COOKIES_BTN)
+    
+
+@then('User is able to see email and password input fields')
+def visibility_of_pass_and_email_fields(context):
+    visibility(context, EMAIL_INPUT_FLD)
+    visibility(context, PASSWORD_INPUT_FLD)
+
+@when('User type proper {email} into email input field')
+def type_in_email(context, email):
+    type(context, EMAIL_INPUT_FLD, email)
+
+@when('User type proper {password} into password input field')
+def type_in_password(context, password):
+    type(context, PASSWORD_INPUT_FLD, password)
+
+@when('User click on Logg inn button')
+def click_on_login_btn(context):
+    click(context, LOGIN_BTN)
